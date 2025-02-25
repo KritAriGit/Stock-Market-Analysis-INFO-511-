2b6e8edab8d1e6ade57ec1f8ad7ddd1dc33684a7
Kritvirya <kritvirya@arizona.edu>
2025-02-25 16:17:37 -0700

    finished fetch script

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -4,7 +4,19 @@ import yfinance as yf
 import pandas as pd
 
 tickers = ['NVDA', 'AAPL', 'TSLA', 'JPM', 'GS']                 # tickers  = symbols for stocks
-start_date = '2020-01-01'                                       
-end_date = '2025-01-01'      
+startD = '2020-01-01'                                       
+endD = '2025-01-01'      
 
+def get_stock(x):
+    print(f"Fetching for {x}...")
+    data = yf.download(x, start = startD, end = endD)
+    file_path = f"data/raw/{x}_stock_data.csv"
+    data.to_csv(file_path)
+    print(f"Saved to {file_path}")
+
+# Fetch data for all tickers
+for ticker in tickers:
+    get_stock(ticker)
+
+print("Data has been successfully fetched.")
 
